9084bada0d4a1cc459c0c1fd18426687ebb5db22
“Andrew <andrew.cheung.2@stonybrook.edu>
2023-06-11 23:40:38 -0400

    website with html css and javascript

---
+++ assets/js/PersonalWebsite.js
@@ -0,0 +1,28 @@
+/**
+ * Author: Andrew Cheung
+ * Date: April 23, 2023
+ * Purpose: JavaScript code for my personal website
+ */
+
+window.onload = function() {
+  document.getElementById("first-tab").click();
+}
+
+
+function openTab(evt, tabName) {
+  // Get all elements with class="tabcontent" and hide them
+  const tabcontent = document.getElementsByClassName("tabcontent");
+  for (let i = 0; i < tabcontent.length; i++) {
+    tabcontent[i].style.display = "none";
+  }
+
+  // Get all elements with class="tablinks" and remove the class "active"
+  const tablinks = document.getElementsByClassName("tablinks");
+  for (let i = 0; i < tablinks.length; i++) {
+    tablinks[i].classList.remove("active");
+  }
+
+  // Show the current tab, and add an "active" class to the button that opened the tab
+  document.getElementById(tabName).style.display = "block";
+  evt.currentTarget.classList.add("active");
+}
